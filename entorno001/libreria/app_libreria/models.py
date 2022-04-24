fddba5505e4aee109cbe38704f183955684acc09
mfcsm <cubillos.marcelo@gmail.com>
2022-04-23 23:32:38 -0400

    cambios base de datos

--- entorno001/libreria/app_libreria/models.py
+++ entorno001/libreria/app_libreria/models.py
@@ -22,7 +22,7 @@ class Libro(models.Model):
     def __str__(self):
         return self.nombre
     
-class Libro(models.Model):
+class Tienda(models.Model):
     id        = models.IntegerField(primary_key=True)
     ubicacion = models.CharField(max_length=50)
     def __str__(self):
