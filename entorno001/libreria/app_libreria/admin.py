f08cb20d7d819cdf0186721f0d6a83cb8e53f24c
mfcsm <cubillos.marcelo@gmail.com>
2022-04-24 00:47:16 -0400

    bd

--- entorno001/libreria/app_libreria/admin.py
+++ entorno001/libreria/app_libreria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Cliente, Libro, Tienda
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Libro)
+admin.site.register(Tienda)
