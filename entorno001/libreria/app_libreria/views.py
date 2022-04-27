29b01d347ef3e96dd0dc720d685a4f0a2f55e6a1
Moon3301 <81126147+Moon3301@users.noreply.github.com>
2022-04-27 14:49:27 -0400

    commit

--- entorno001/libreria/app_libreria/views.py
+++ entorno001/libreria/app_libreria/views.py
@@ -8,12 +8,27 @@ def inicio (request):
 def libros (request):
     return render (request, 'libros.html')
 
+def libros1 (request):
+    return render (request, 'detalle_libros.html')
+
+
 
 def listarLibros (request):
     listado = Libro.objects.all()
 
     contexto = {'listado' : listado}
     return render (request, 'libros.html', contexto)
+
+
+def detalleLibros (request, id):
+    
+    libro = Libro.objects.get(id = id)
+    
+    contexto = {'Libro' : LibroForm(instance=libro)}
+    
+ 
+    
+    return render (request, 'detalle_libros.html',contexto)
     
 
 
